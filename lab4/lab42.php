c47131adb86f890ff4759d33c35eb28bd27b5f32
emontilla07 <emontilla107@gmail.com>
2023-09-20 09:14:32 -0500

    Lab4 complete

--- lab4/lab42.php
+++ lab4/lab42.php
@@ -1,10 +1,39 @@
-<?php
-    $number1 = $_REQUEST['number1'];
-    $result = 1;
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lab 4.2</title>
+</head>
+<body>
+    <!-- Serie fibonacci -->
+    <h1>Serie Fibonacci</h1>
 
-    for ($i = 1; $i <= $number1; $i++) {
-        $result *= $i;
-    }
+    <form action="lab42.php" method="post">
+        <label for="numero">Ingrese un número: </label>
+        <input type="number" name="numero" id="numero" required>
+        <input type="submit" value="Calcular">
+    </form>
 
-    echo "El valor factorial del número $number1 es igual a $result";
-?>
+    <?php
+        if (isset($_POST['numero'])) {
+            $numero = $_POST['numero'];
+            $a = 0;
+            $b = 1;
+            $c = 0;
+            echo "<p>La serie Fibonacci de $numero es: </p>";
+            echo "<p>$a, $b, ";
+            for ($i = 0; $i < $numero - 2; $i++) {
+                $c = $a + $b;
+                $a = $b;
+                $b = $c;
+                echo "$c, ";
+            }
+            echo "</p>";
+        }
+    ?>
+
+
+</body>
+</html>
+</html>
