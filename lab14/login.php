f08b1feea733ec19a0e53c3de214ed3b4d309f5e
emontilla07 <emontilla107@gmail.com>
2023-10-25 00:02:39 -0500

    done lab 14

--- lab14/login.php
+++ lab14/login.php
@@ -10,8 +10,8 @@
 
         require_once('class/usuarios.php');
 
-        $obj_usuario = new Usuarios();
-        $usuario_validado = $obj_usuario->validarUsuario($usuario, $clave_crypt);
+        $obj_usuarios = new Usuarios();
+        $usuario_validado = $obj_usuarios->validarUsuario($usuario, $clave_crypt);
 
         foreach ($usuario_validado as $array_resp) {
             foreach ($array_resp as $value) {
@@ -20,14 +20,14 @@
         }
 
         if ($nfilas > 0) {
-            $usuario_validado = $usuario;
-            $_SESSION['usuario_valido'] = $usuario_validado;
+            $usuario_valido = $usuario;
+            $_SESSION["usuario_valido"] = $usuario_valido;
         }
     }
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -36,40 +36,40 @@
 </head>
 <body>
     <?php
-        // sesión iniciada
-        if (isset($_SESSION['usuario_validado'])) {
+        if (isset($_SESSION['usuario_valido'])) {
     ?>
-    <h1>Gestión de Noticias</h1>
+    <h1>Gestion de Noticias</h1>
     <hr>
+
     <ul>
         <li><a href="lab142.php">Listar todas las noticias</a></li>
-        <li><a href="lab143.php">Listar noticias por partes</li>
-        <li><a href="lab144.php">Buscar noticias</li>
+        <li><a href="lab143.php">Listar noticias por partes</a></li>
+        <li><a href="lab144.php">Buscar noticias</a></li>
     </ul>
     <hr>
-    <p><a href="logout.php">Desconectar</a></p>
-    <?php
-        }
 
+    <p>[ <a href="logout.php">Desconectar</a> ]</p>
+    <?php
         // Intento de entrada fallido
-        else if (isset($usuario)) {
+        } elseif (isset($usuario)) {
             print("<br><br>\n");
             print("<p align='center'>Acceso no autorizado</p>\n");
-            print("<p align='center'><a href='login.php'>Conectar</a></p>\n");
-        } else {   // Sesíon no iniciada
+            print("<p align='center'>[ <a href='login.php'>Conectar</a> ]</p>");
+        } else {
             print("<br><br>\n");
-            print("<p class='parrafocentrado'>Esta zona tiene el acceso restringido.<br> " .
-                " Para entrar debe identificarse</p>");
+            print("<p class='parrafocentrado'>Esta zona tiene el acceso restringido.<br>" .
+                " Para entrar debe identificarse</p>\n");
             print("<form class='entrada' name='login' action='login.php' method='post'>\n");
-                print("<p><label class='etiqueta-entrada'>Usuario: </label>\n");
-                print("<input type='text' name='usuario' size='15'></p>\n");
-                print("<p><label class='etiqueta-entrada'>Clave: </label>\n");
-                print("<input type='password' name='clave' size='15'></p>\n");
-                print("<p><input type='submit' value='Entrar' name='entrar'></p>\n");
+                print("<p><label class='etiqueta-entrada'>Usuario:</label>\n");
+                print("    <input type='text' name='usuario' size='15'></p>\n");
+                print("<p><label class='etiqueta-entrada'>Clave:</label>\n");
+                print("    <input type='password' name='clave' size='15'></p>\n");
+                print("<p><input type='submit' value='Entrar'></p>\n");
             print("</form>\n");
-            print("<p class='parrofocentrado'>NOTA: si no dispone de identificación o tiene problemas " .
-                 "para entrar<br>pongase en contacto con el " .
-                 "<a href='mailto:webmaster@localhost'>Administrador</a> del sitio</p>\n");
+
+            print("<p class='parrafocentrado'>NOTA: si no dispone de identificación o tiene problemas " .
+                "para entrar<br>pongase en contacto con el " . 
+                "<a href='mailto:webmaster@localhost'>administrador</a> el sitio</p>\n");
         }
     ?>
 </body>
--- lab14/logout.php
+++ lab14/logout.php
@@ -3,24 +3,24 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desconectar</title>
-    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="css/estilo.css"
 </head>
 <body>
     <?php
-        if (isset($_SESSION['usuario_validado'])) {
+        if (isset($_SESSION['usuario_valido'])) {
             session_destroy();
 
             print("<br><br><p align='center'>Conexión finalizada</p>\n");
-            print("<p align='center'><a href='login.php'>Conectar</a></p>\n");
+            print("<p align='center'>[ <a href='login.php'>Conectar</a> ]</p>\n");
         } else {
             print("<br><br>\n");
             print("<p align='center'>No existe una conexión activa</p>\n");
-            print("<p align='center'><a href='login.php'>Conectar</a></p>\n");
+            print("<p align='center'>[ <a href='login.php'>Conectar</a> ]</p>\n");
         }
     ?>
 </body>
