ecaa2cf84a3a35024b44bbab3d98939852b9f32e
emontilla07 <72420038+emontilla07@users.noreply.github.com>
2023-12-12 13:27:03 -0500

    Update index.php

--- final-project/index.php
+++ final-project/index.php
@@ -3,7 +3,6 @@
         <div class="container-fluid py-5">
           <h1 class="display-5 fw-bold">Bienvenid@ al sistema</h1>
           <p class="col-md-8 fs-4">Usuario: <?php echo $_SESSION['user']; ?></p>
-          <button class="btn btn-primary btn-lg" type="button">Example button</button>
         </div>
       </div>
 <?php include("templates/footer.php"); ?>
